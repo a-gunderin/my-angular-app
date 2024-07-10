b13882cf794913769d0727b10913788b223f206a
Andrei Gunderin <ga.webnotes@gmail.com>
2024-07-10 03:02:23 +0300

    12-forms

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housingLocation';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
@@ -16,8 +17,19 @@ export class DetailsComponent {
   housingLocationId = -1;
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
   constructor(){
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '');
   }
 }
