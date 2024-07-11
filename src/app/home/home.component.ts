23b166be1090d9a28cbab129f37ea5569b92eb4c
Andrei Gunderin <ga.webnotes@gmail.com>
2024-07-11 03:45:20 +0300

    13-search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,20 +3,37 @@ import {CommonModule} from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housingLocation';
 import { HousingService } from '../housing.service';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   housingService: HousingService  = inject(HousingService);
+  housingLocationList!: HousingLocation[];
+  filteredLocationList: HousingLocation[] = [];
+  filterForm = new FormGroup({
+    inputValue: new FormControl(''),
+  });
 
   constructor() {
     this.housingLocationList = this.housingService.getHousingLocationList();
+    this.filteredLocationList = this.housingLocationList;
   }
 
-  housingLocationList!: HousingLocation[];
+  filterResults() {
+    const value =this.filterForm.value.inputValue; 
+    if (!value) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList
+    .filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(value.toLowerCase())
+    );
+  }
 }
